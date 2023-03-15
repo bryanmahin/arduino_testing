d041c393f56b9eb836afab02909379c6888d0a0b
bryanmahin <bryan.mahin@gmail.com>
2023-03-15 14:14:54 -0400

    removed if else

--- Projects/ArduinoTutorial42/src/main.cpp
+++ Projects/ArduinoTutorial42/src/main.cpp
@@ -3,9 +3,9 @@ int latchPin=11;
 int clockPin=9;
 int dataPin=12;
 int count=1;
-int dt=250;
+int dt=125;
 
-byte myNumb=0b00000001; //0b+binary 
+byte myNumb=0b00000000; //0b+binary 
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,18 +19,10 @@ pinMode(clockPin,OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 
-if (myNumb == 255) 
-  { 
-    myNumb=0; 
-  } else
-  {
-    // Serial.println(myNumb);
-    myNumb=myNumb+1;
-  }
-
 digitalWrite(latchPin,LOW);
 shiftOut(dataPin,clockPin,LSBFIRST,myNumb);
 digitalWrite(latchPin,HIGH);
 delay(dt);
+myNumb=myNumb+1;
 
 }
