41fe42a4b34f530c6214f4864b0fc42b0c02a6b4
bryanmahin <bryan.mahin@gmail.com>
2023-03-14 21:51:51 -0400

    Tutorial41

--- Projects/ArduinoTutorial40/src/main.cpp
+++ Projects/ArduinoTutorial40/src/main.cpp
@@ -2,29 +2,22 @@
 int speedPin=5;
 int dir1=4;
 int dir2=3;
-int mSpeed=90;
-int mStart=150;
-int mStartDelay=50;
-int joyx=A0;
-int xVal;
-int upPin=8;
-int downPin=9;
-int upVal;
-int downVal;
-int dt=10;
-int fanDir=500; //1-499 is reverse, 500 is stopped, 501-1000 forward.
-
+int BP1=8;
+int BP2=9;
+int B1Val;
+int B2Val;
+int mSpeed=0;
+int dt=500;
 
 void setup() {
   // put your setup code here, to run once:
 pinMode(speedPin,OUTPUT);
 pinMode(dir1,OUTPUT);
 pinMode(dir2,OUTPUT);
-pinMode(joyx,INPUT);
-pinMode(upPin,OUTPUT);
-pinMode(downPin,OUTPUT);
-digitalWrite(upPin,HIGH);
-digitalWrite(downPin,HIGH);
+pinMode(BP1,INPUT);
+pinMode(BP2,INPUT);
+digitalWrite(BP1,HIGH);
+digitalWrite(BP2,HIGH);
 Serial.begin(9600);
 }
 
@@ -32,112 +25,61 @@ Serial.begin(9600);
 void loop() {
   // put your main code here, to run repeatedly:
 
-upVal=digitalRead(upPin);
-downVal=digitalRead(downPin);
-
-if (fanDir == 500)
-    {
-      digitalWrite(dir1,HIGH);
-      digitalWrite(dir2,LOW);
-      analogWrite(speedPin,0);
-      Serial.print("mSpeed:");
-      Serial.print(mSpeed);
-      Serial.print(" ");
-      Serial.print("fanDir:");
-      Serial.println(fanDir);
-    }
-
-if (upVal==0 && fanDir >= 500 && fanDir <= 664) 
-  { 
-
-    digitalWrite(dir1,HIGH);
-    digitalWrite(dir2,LOW);
-    if (mSpeed < 95)
-    {
-      analogWrite(speedPin,mStart);
-      delay(mStartDelay);
-    }
-    analogWrite(speedPin,mSpeed);
-    mSpeed=mSpeed+1;
-    fanDir=fanDir+1;
-    Serial.print("mSpeed:");
-    Serial.print(mSpeed);
-    Serial.print(" ");
-    Serial.print("fanDir:");
-    Serial.println(fanDir);
-  } 
-
-if (downVal==0 && fanDir >= 501 && fanDir <= 665) 
-  { 
-    digitalWrite(dir1,HIGH);
-    digitalWrite(dir2,LOW);
-    analogWrite(speedPin,mSpeed);
-    mSpeed=mSpeed-1;
-    fanDir=fanDir-1;
-    Serial.print("mSpeed:");
-    Serial.print(mSpeed);
-    Serial.print(" ");
-    Serial.print("fanDir:");
-    Serial.println(fanDir);
-  } 
+B1Val=digitalRead(BP1);
+B2Val=digitalRead(BP2);
+Serial.print("Motor Speed ");
+Serial.println(mSpeed);
 
 
-  // if (downVal == 0 && fanDir > 500 && fanDir <= 665) 
-  // { 
-  //   digitalWrite(dir1,HIGH);
-  //   digitalWrite(dir2,LOW);
-  //   analogWrite(speedPin,mSpeed);
-  //   delay(dt);
-  //   mSpeed=mSpeed-1;
-  //   fanDir=fanDir-1;
-  //   Serial.print("mSpeed:");
-  //   Serial.print(mSpeed);
-  //   Serial.print(" ");
-  //   Serial.print("fanDir:");
-  //   Serial.println(fanDir);
-  //   if (mSpeed == 89)
-  //   {
-  //     mSpeed=0;
-  //   }
-    
-  // } 
+if (B1Val == 0) { 
+  mSpeed=mSpeed-10;
+  delay(dt);
+}
 
-  
+if (B2Val == 0) {
+  mSpeed=mSpeed+10;
+  delay(dt);
+}
 
+if (mSpeed>255){
+  mSpeed=255;
+}
 
-// if (downVal == 0 && mSpeed > 0)
-// {
-//   Serial.println(mSpeed);
-//     digitalWrite(dir1,HIGH);
-//     digitalWrite(dir2,LOW);
-//     analogWrite(speedPin,mSpeed);
-//     delay(dt);
-//     mSpeed=mSpeed-1;
-// }
+if (mSpeed<-255){
+  mSpeed=-255;
+}
 
+if (mSpeed==10) {
+  mSpeed=100;
+}
 
+if (mSpeed==-10) {
+  mSpeed=-100;
+}
 
-// xVal=analogRead(joyx);
-// if (xVal > 513 && xVal < 517)
-// {
-//   digitalWrite(dir1,HIGH);
-//   digitalWrite(dir2,LOW);
-//   analogWrite(speedPin,0);
-//   Serial.println(xVal);
-// }
-// if (xVal < 513)
-// {
-//   mSpeed=map(xVal, 513, 0, 83, 255);
-//   analogWrite(speedPin,mSpeed);
-//   Serial.println(xVal);
-// }
-// if (xVal > 517)
-// {
-//   digitalWrite(dir1,LOW);
-//   digitalWrite(dir2,HIGH);
-//   mSpeed=map(xVal, 517, 1023, 83, 255);
-//   analogWrite(speedPin,mSpeed);
-//   Serial.println(xVal);
-// }
+if (mSpeed==90 || mSpeed==95) {
+  mSpeed=0;
+}
+
+if (mSpeed==-90 || mSpeed==-95) {
+  mSpeed=0;
+}
+
+if (mSpeed==0) {
+analogWrite(speedPin,0);
+}
+
+if (mSpeed>0) {
+digitalWrite(dir1,HIGH);
+digitalWrite(dir2,LOW);
+analogWrite(speedPin,mSpeed);
+}
+
+if (mSpeed<0) {
+digitalWrite(dir1,LOW);
+digitalWrite(dir2,HIGH);
+analogWrite(speedPin,abs(mSpeed));
+}
+
 
 }
