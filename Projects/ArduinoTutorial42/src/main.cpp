22d89a3d581db65ad5c99b118231c2b3fa859d03
bryanmahin <bryan.mahin@gmail.com>
2023-03-15 14:04:27 -0400

    finished tutorial

--- Projects/ArduinoTutorial42/src/main.cpp
+++ Projects/ArduinoTutorial42/src/main.cpp
@@ -2,8 +2,10 @@
 int latchPin=11;
 int clockPin=9;
 int dataPin=12;
+int count=1;
+int dt=250;
 
-byte LEDs=0xF5;
+byte myNumb=0b00000001; //0b+binary 
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,8 +19,18 @@ pinMode(clockPin,OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 
+if (myNumb == 255) 
+  { 
+    myNumb=0; 
+  } else
+  {
+    // Serial.println(myNumb);
+    myNumb=myNumb+1;
+  }
+
 digitalWrite(latchPin,LOW);
-shiftOut(dataPin,clockPin,LSBFIRST,LEDs);
+shiftOut(dataPin,clockPin,LSBFIRST,myNumb);
 digitalWrite(latchPin,HIGH);
+delay(dt);
 
 }
