26c0c052216d03e2474a0ef0d21e4d1c41fc4d0e
bryanmahin <bryan.mahin@gmail.com>
2023-04-25 09:10:07 -0400

    commit

--- Projects/CANBusTest2/src/main.cpp
+++ Projects/CANBusTest2/src/main.cpp
@@ -1,43 +1,34 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <mcp2515.h>
+#include <CanHacker.h>
+#include <mcp2515_can.h>
 
+// Set SPI CS Pin according to your hardware
 const int SPI_CS_PIN = 17;
 const int CAN_INT_PIN = 2;
 
-const uint32_t CAN_ID_DOOR_STATUS = 0x0809;
-const uint8_t SLIDING_DOOR_BYTE_INDEX = 6;
+mcp2515_can CAN(SPI_CS_PIN);
 
-const uint8_t SLIDING_DOOR_OPEN = 1;
-const uint8_t SLIDING_DOOR_CLOSED = 0;
-
-MCP2515 mcp2515(SPI_CS_PIN);
+CanHacker canHacker(&CAN);
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) {};
+  while (!Serial) {}
 
-  SPI.begin();
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_16MHZ);
-  mcp2515.setNormalMode();
-
-  pinMode(CAN_INT_PIN, INPUT);
+  while (CAN_OK != CAN.begin(CAN_500KBPS)) {
+    Serial.println(F("CAN init fail, retry..."));
+    delay(100);
+  }
+  Serial.println(F("CAN init ok!"));
+  canHacker.begin();
 }
 
 void loop() {
-  if (digitalRead(CAN_INT_PIN) == LOW) {
-    CANMSG msg;
-    mcp2515.readMessage(&msg);
-
-    if (msg.can_id == CAN_ID_DOOR_STATUS) {
-      uint8_t sliding_door_state = msg.data[SLIDING_DOOR_BYTE_INDEX];
-
-      if (sliding_door_state == SLIDING_DOOR_OPEN) {
-        Serial.println("Sliding door is open.");
-      } else if (sliding_door_state == SLIDING_DOOR_CLOSED) {
-        Serial.println("Sliding door is closed.");
-      }
-    }
+  canHacker.tick();
+  if (Serial.available()) {
+    canHacker.sendSerial(Serial.read());
+  }
+  if (canHacker.available()) {
+    Serial.write(canHacker.recvSerial());
   }
 }
